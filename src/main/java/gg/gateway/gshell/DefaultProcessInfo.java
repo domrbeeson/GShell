5dcfc69280f4b374eb2a04aebd0c0b5519491a21
Dominic Beeson <dominicraebeeson@gmail.com>
2021-07-04 17:25:39 +0100

    store start time

--- src/main/java/gg/gateway/gshell/DefaultProcessInfo.java
+++ src/main/java/gg/gateway/gshell/DefaultProcessInfo.java
@@ -3,6 +3,7 @@ package gg.gateway.gshell;
 import gg.gateway.gshell.interfaces.GProcessInfo;
 import org.bukkit.command.CommandSender;
 
+import java.util.Date;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -12,6 +13,7 @@ public class DefaultProcessInfo implements GProcessInfo {
     private final String[] args;
     private final Process process;
     private final HashSet<CommandSender> listeners = new HashSet<>();
+    private final Date startTime = new Date();
 
     public DefaultProcessInfo(Process process, CommandSender executor, String...args) {
         this.process = process;
@@ -23,6 +25,7 @@ public class DefaultProcessInfo implements GProcessInfo {
     public String[] getArgs() { return args; }
     public Process getProcess() { return process; }
     public Set<CommandSender> getOutputListeners() { return listeners; }
+    public Date getStartTime() { return startTime; }
 
     public boolean registerOutputListener(CommandSender listener) { return listeners.add(listener); }
     public boolean unregisterOutputListener(CommandSender listener) { return listeners.remove(listener); }
