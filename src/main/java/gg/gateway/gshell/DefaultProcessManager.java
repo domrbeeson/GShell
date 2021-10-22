d32ec188a5192d45cc4f0c722a631b5277ee41a1
Dom <42182167+domrbeeson@users.noreply.github.com>
2021-10-22 19:25:55 +0100

    support using java9+ process.pid() method

--- src/main/java/gg/gateway/gshell/DefaultProcessManager.java
+++ src/main/java/gg/gateway/gshell/DefaultProcessManager.java
@@ -15,125 +15,128 @@ import java.util.concurrent.ConcurrentHashMap;
 
 public class DefaultProcessManager implements GProcessManager {
 
-    private final ConcurrentHashMap<Long, GProcessInfo> processes = new ConcurrentHashMap<>();
-    private final Plugin plugin;
-    private final BukkitScheduler scheduler;
+	private final ConcurrentHashMap<Long, GProcessInfo> processes = new ConcurrentHashMap<>();
+	private final Plugin plugin;
+	private final BukkitScheduler scheduler;
 
-    private long programCounter = 0;
+	private long programCounter = 0;
 
-    public DefaultProcessManager(Plugin plugin, BukkitScheduler scheduler) {
-        this.plugin = plugin;
-        this.scheduler = scheduler;
-    }
+	public DefaultProcessManager(Plugin plugin, BukkitScheduler scheduler) {
+		this.plugin = plugin;
+		this.scheduler = scheduler;
+	}
 
-    public long startProcess(CommandSender executor, boolean autoListenToOutput, String...args) {
+	public long startProcess(CommandSender executor, boolean autoListenToOutput, String...args) {
 
-        ProcessBuilder builder = new ProcessBuilder(args);
-        builder.redirectErrorStream(true);
-        Process process;
-        final Date startTime = new Date();
-        try {
-            process = builder.start();
-        } catch (IOException e) {
-            return -1;
-        }
+		ProcessBuilder builder = new ProcessBuilder(args);
+		builder.redirectErrorStream(true);
+		Process process;
+		final Date startTime = new Date();
+		try {
+			process = builder.start();
+		} catch (IOException e) {
+			return -1;
+		}
 
-        // java 8 doesn't have process.pid() so just increment program counter instead
-        // it's probably certain that nobody will reach the 64 bit limit
-        programCounter++;
-        long id = programCounter;
+		// because java 8 could be used, check whether process.pid() exists
+		// if it doesn't then just increment the program counter for a basic pid
+		long pid = -1;
+		try {
+			pid = process.pid();
+		} catch (Exception e) {}
+		final long id = pid != -1 ? pid : programCounter++;
 
-        GProcessInfo info = new DefaultProcessInfo(
-                process,
-                executor,
-                args
-        );
-        processes.put(id, info);
+		GProcessInfo info = new DefaultProcessInfo(
+				process,
+				executor,
+				args
+		);
+		processes.put(id, info);
 
-        // handle process output in new thread
-        scheduler.runTaskAsynchronously(plugin, () -> {
+		// handle process output in new thread
+		scheduler.runTaskAsynchronously(plugin, () -> {
 
-            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
-            PrintWriter logWriter = null;
+			BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+			PrintWriter logWriter = null;
 
-            String line;
-            try {
+			String line;
+			try {
 
-                boolean isPlayer = executor instanceof Player;
-                String executorId = isPlayer ? ((Player) executor).getUniqueId().toString() : "CONSOLE";
-                String dateString = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(startTime);
-                String logName = executorId + " " + dateString;
+				boolean isPlayer = executor instanceof Player;
+				String executorId = isPlayer ? ((Player) executor).getUniqueId().toString() : "CONSOLE";
+				String dateString = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(startTime);
+				String logName = executorId + " " + dateString;
 
-                logWriter = new PrintWriter(new File(GShell.PROCESS_LOGS_FOLDER + "/" + logName + ".txt"));
-                logWriter.println("Executor: " + executorId + (isPlayer ? " (" + ((Player) executor).getName() + ")" : ""));
-                logWriter.println("Run date: " + dateString);
-                logWriter.println("Command:  " + String.join(" ", args));
-                logWriter.println("Output:");
+				logWriter = new PrintWriter(new File(GShell.PROCESS_LOGS_FOLDER + "/" + logName + ".txt"));
+				logWriter.println("Executor: " + executorId + (isPlayer ? " (" + ((Player) executor).getName() + ")" : ""));
+				logWriter.println("Run date: " + dateString);
+				logWriter.println("Command:  " + String.join(" ", args));
+				logWriter.println("Output:");
 
-                while ((line = reader.readLine()) != null) {
+				while ((line = reader.readLine()) != null) {
 
-                    final String outputLine = line;
+					final String outputLine = line;
 
-                    info.getOutputListeners().forEach(listener -> listener.sendMessage(outputLine));
-                    logWriter.println(line);
+					info.getOutputListeners().forEach(listener -> listener.sendMessage(outputLine));
+					logWriter.println(line);
 
-                }
+				}
 
-                process.waitFor();
+				process.waitFor();
 
-            } catch (IOException | InterruptedException e) {
-                e.printStackTrace();
-            } finally {
+			} catch (IOException | InterruptedException e) {
+				e.printStackTrace();
+			} finally {
 
-                // process has exited, do cleanup
-                if (logWriter != null) logWriter.close();
+				// process has exited, do cleanup
+				if (logWriter != null) logWriter.close();
 
-                String message = GShellMessages.PROCESS_EXIT_MESSAGE(id, process.exitValue());
+				String message = GShellMessages.PROCESS_EXIT_MESSAGE(id, process.exitValue());
 
-                info.registerOutputListener(info.getExecutor()); // make sure person that executed the command knows the process has ended, even if they ran it quietly
-                info.getOutputListeners().forEach(listener -> listener.sendMessage(message));
+				info.registerOutputListener(info.getExecutor()); // make sure person that executed the command knows the process has ended, even if they ran it quietly
+				info.getOutputListeners().forEach(listener -> listener.sendMessage(message));
 
-                processes.remove(id);
-            }
+				processes.remove(id);
+			}
 
-        });
+		});
 
-        return id;
-    }
+		return id;
+	}
 
-    public boolean stopProcess(long id, boolean force) {
-        if (!isIdRunning(id)) return true;
+	public boolean stopProcess(long id, boolean force) {
+		if (!isIdRunning(id)) return true;
 
-        Process process = processes.get(id).getProcess();
-        if (!process.isAlive()) {
-            processes.remove(id);
-            return true;
-        }
+		Process process = processes.get(id).getProcess();
+		if (!process.isAlive()) {
+			processes.remove(id);
+			return true;
+		}
 
-        if (force) process.destroyForcibly();
-        else process.destroy();
+		if (force) process.destroyForcibly();
+		else process.destroy();
 
-        try {
-            process.waitFor();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-            return false;
-        }
+		try {
+			process.waitFor();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+			return false;
+		}
 
-        processes.remove(id);
-        return true;
-    }
+		processes.remove(id);
+		return true;
+	}
 
-    public GProcessInfo getProcessInfo(long id) {
-        return processes.get(id);
-    }
+	public GProcessInfo getProcessInfo(long id) {
+		return processes.get(id);
+	}
 
-    public boolean isIdRunning(long id) {
-        return processes.containsKey(id);
-    }
+	public boolean isIdRunning(long id) {
+		return processes.containsKey(id);
+	}
 
-    public Map<Long, GProcessInfo> listRunningProcesses() {
-        return processes;
-    }
+	public Map<Long, GProcessInfo> listRunningProcesses() {
+		return processes;
+	}
 
 }
